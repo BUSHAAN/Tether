b0692432f5f9eb506cc403e6ac0c15458e9bdaf4
BUSHAAN <bushaangunatilake@gmail.com>
2025-04-02 03:27:58 +0530

    add tether package; update package.json and package-lock.json; configure axios base URL for development and production environments

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,14 @@ import messageRoutes from "./routes/message.route.js";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import { connectDB } from "./lib/db.js";
-import {app,server} from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
 dotenv.config();
 
-
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
 app.use(
   express.json({
     limit: "50mb",
@@ -20,6 +22,13 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log("Server running on port: ", PORT);
--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -5,7 +5,7 @@ import toast from "react-hot-toast";
 import { AxiosError } from "axios";
 import { io, Socket } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_SOCKET_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000" : "/";
 
 interface useAuthStoreType {
   authUser: User | null;
