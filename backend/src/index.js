7a4fbaf24466c797b40bd8f9540988a49368b141
BUSHAAN <bushaangunatilake@gmail.com>
2025-04-20 23:14:53 +0530

    fix file path in express static middleware; correct path to frontend dist directory

--- backend/src/index.js
+++ backend/src/index.js
@@ -24,13 +24,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  
+
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
   });
 }
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port: ", PORT);
   connectDB();
 });
